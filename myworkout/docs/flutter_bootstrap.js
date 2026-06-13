12fc39446ad4737678ef6c83cb302481145ad521
gio68rm <gio68rm@gmail.com>
2026-06-13 15:21:46 +0000

    Deploy complete Flutter web build

--- myworkout/docs/flutter_bootstrap.js
+++ myworkout/docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2556390734" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3097389594" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
