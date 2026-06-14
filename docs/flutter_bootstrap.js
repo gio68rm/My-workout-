7ee81e63bf99f199467cf17e8dc0302d49bdd7aa
gio68rm <gio68rm@gmail.com>
2026-06-14 19:42:01 +0000

    Fix splash page definitiva

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "653164190" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1339854349" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- myworkout/lib/pages/splash_page.dart
+++ myworkout/lib/pages/splash_page.dart
@@ -9,24 +9,34 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
+  bool _loading = false;
+
+  void _goToDashboard() async {
+    setState(() => _loading = true);
+    await Future.delayed(const Duration(milliseconds: 800));
+    if (mounted) {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (_) => const MainNavigation()),
       );
-    });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
-        child: Image(
-          image: AssetImage('assets/logo.png'),
-          width: 180,
+        child: GestureDetector(
+          onTap: _loading ? null : _goToDashboard,
+          child: AnimatedOpacity(
+            opacity: _loading ? 0.5 : 1.0,
+            duration: const Duration(milliseconds: 300),
+            child: Image.asset(
+              'assets/logo.png',
+              width: 180,
+            ),
+          ),
         ),
       ),
     );
